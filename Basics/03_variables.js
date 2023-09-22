82789dd0cb379d7e2076b75a6b552276865aeac4
Rahulpandey1236 <pandeyrahul8218@gmail.com>
2023-09-22 23:06:45 +0530

    first commit

---
+++ Basics/03_variables.js
@@ -0,0 +1,8 @@
+let accountEmail="rahul@123";
+var accountPassword="124";
+const id=101;
+
+accountEmail="shyam123";
+accountPassword="456";
+
+console.table([accountEmail,accountPassword,id]);
---
+++ Basics/04_Datatypes.js
@@ -0,0 +1,25 @@
+"use strict";//traet all code as newer version
+
+//alert(3 + 3) we are using node not browser
+
+console.log(3 + 3);
+let name="Rahul"
+let age=19;
+let isloggedin=false;
+let state;
+
+//number ==> 2 to power 53
+//bigint
+//string
+//boolean
+//null ==> standalone vlaue
+//undefined
+//symbol
+
+
+
+
+//object
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
